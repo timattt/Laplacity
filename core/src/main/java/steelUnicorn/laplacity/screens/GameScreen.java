fad9647b9aa9e42cb1fd992436f4c9b91cbbf554
EKhunshaev <elveg100@gmail.com>
2022-07-15 11:17:37 +0300

    ui/user interface functionality

--- core/src/main/java/steelUnicorn/laplacity/screens/GameScreen.java
+++ core/src/main/java/steelUnicorn/laplacity/screens/GameScreen.java
@@ -2,6 +2,7 @@ package steelUnicorn.laplacity.screens;
 
 import static steelUnicorn.laplacity.GameProcess.*;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 
 import steelUnicorn.laplacity.Globals;
@@ -9,13 +10,25 @@ import steelUnicorn.laplacity.LaplacityAssets;
 import steelUnicorn.laplacity.ui.GameInterface;
 
 public class GameScreen extends ScreenAdapter {
-	GameInterface gi;
-	
+	private GameInterface gi;
+
 	public GameScreen() {
 		initLevel(LaplacityAssets.LEVEL1_TILEMAP);
 		gi = new GameInterface(Globals.guiViewport);
 	}
-	
+
+	@Override
+	public void show() {
+		Gdx.input.setInputProcessor(gi);
+		super.show();
+	}
+
+	@Override
+	public void hide() {
+		Gdx.input.setInputProcessor(null);
+		super.hide();
+	}
+
 	@Override
 	public void render(float delta) {
 		if (!isPlaying()) {
--- core/src/main/java/steelUnicorn/laplacity/ui/GameInterface.java
+++ core/src/main/java/steelUnicorn/laplacity/ui/GameInterface.java
@@ -2,73 +2,114 @@ package steelUnicorn.laplacity.ui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import steelUnicorn.laplacity.GameProcess;
 import steelUnicorn.laplacity.Globals;
+import steelUnicorn.laplacity.field.GameMode;
 
+/**
+ * @brief Class that creates game ui and add listeners.
+ *
+ * When any icon clicked, GameProcess.currentGameMode change its state
+ *
+ */
 public class GameInterface extends Stage {
+	/**
+	 * Constructor create Stage and interface
+	 * @param viewport
+	 */
 	public GameInterface(Viewport viewport) {
 		super(viewport);
-		
+
 		createInterface();
 	}
 	
+	/**
+	 * @brief Function creates interface buttons
+	 *
+	 * The order is
+	 * <ol>
+	 * 	<li>return</li>
+	 * 	<li>reload</li>
+	 * 	<li>flight</li>
+	 * 	<li>eraser</li>
+	 * 	<li>electrons</li>
+	 * 	<li>protons</li>
+	 * 	<li>dirichlet</li>
+	 * </ol>
+	 */
 	private void createInterface() {
 		Table root = new Table();
 		root.setFillParent(true);
 		root.align(Align.right);
 		addActor(root);
-		
+
 		TextureAtlas icons = Globals.assetManager.get("ui/gameicons/icons.atlas", TextureAtlas.class);
-		
-		Drawable img = new TextureRegionDrawable(icons.findRegion("return"));
-		ImageButton btn = new ImageButton(img);
-		btn.setName("return");
+
+		//reload and return buttons
+		createIcon(icons, "return", root, new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Gdx.app.log("game ui", "return pressed");
+				Globals.game.setScreen(Globals.mainMenuScreen);
+			}
+		});
+
+		createIcon(icons, "reload", root, new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				Gdx.app.log("game ui", "reload pressed");
+				//TODO level reload
+			}
+		});
+
+		//modes
+		createModeIcon(icons, "flight", root, GameMode.flight);
+		createModeIcon(icons, "eraser", root, GameMode.eraser);
+		createModeIcon(icons, "electrons", root, GameMode.electrons);
+		createModeIcon(icons, "protons", root, GameMode.protons);
+		createModeIcon(icons, "dirichlet", root, GameMode.dirichlet);
+	}
+
+	/**
+	 * Function that creates 1 icon button of game interface
+	 *
+	 * @param icons - TextureAtlas with icons in it
+	 * @param name - name of TextureRegion in Atlas
+	 * @param root - table where button is placed
+	 * @param listener - listener that define buttons behaviour
+	 */
+	private void createIcon(TextureAtlas icons, String name, Table root, ChangeListener listener) {
+		ImageButton btn = new ImageButton(new TextureRegionDrawable(icons.findRegion(name)));
+		btn.setName(name);
+		btn.addListener(listener);
 		root.add(btn);
 		root.row();
-		
-		
-		img = new TextureRegionDrawable(icons.findRegion("reload"));
-		btn = new ImageButton(img);
-		btn.setName("reload");
-		root.add(btn);
-		root.row();
-		
-		img = new TextureRegionDrawable(icons.findRegion("start"));
-		btn = new ImageButton(img);
-		btn.setName("start");
-		root.add(btn);
-		root.row();
-		
-		img = new TextureRegionDrawable(icons.findRegion("eraser"));
-		btn = new ImageButton(img);
-		btn.setName("eraser");
-		root.add(btn);
-		root.row();
-		
-		img = new TextureRegionDrawable(icons.findRegion("electron"));
-		btn = new ImageButton(img);
-		btn.setName("electron");
-		root.add(btn);
-		root.row();
-		
-		img = new TextureRegionDrawable(icons.findRegion("proton"));
-		btn = new ImageButton(img);
-		btn.setName("proton");
-		root.add(btn);
-		root.row();
-		
-		img = new TextureRegionDrawable(icons.findRegion("dirich"));
-		btn = new ImageButton(img);
-		btn.setName("dirich");
-		root.add(btn);
-		root.row();
+	}
+
+	/**
+	 * Function that creates mode button
+	 *
+	 * @param icons - Texture Atlas with icons of modes
+	 * @param name - name of mode
+	 * @param root - table where button is placed
+	 * @param mode - mode that button enables
+	 */
+	private void createModeIcon(TextureAtlas icons, String name, Table root, GameMode mode) {
+		createIcon(icons, name, root, new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				GameProcess.currentGameMode = mode;
+				Gdx.app.log("game ui", GameProcess.currentGameMode.toString());
+			}
+		});
 	}
 }
