9be462b6a64dfee6e4835c9429b657fda86f04e9
teldufalsari <idunaev71@gmail.com>
2022-07-25 14:53:58 +0300

    Add laser cages rendering

--- core/src/main/java/steelUnicorn/laplacity/core/LaplacityAssets.java
+++ core/src/main/java/steelUnicorn/laplacity/core/LaplacityAssets.java
@@ -2,6 +2,7 @@ package steelUnicorn.laplacity.core;
 
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.files.FileHandle;
