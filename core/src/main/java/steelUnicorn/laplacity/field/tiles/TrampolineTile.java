570febd09ceddadc7082362412d9f6c5599fe857
timattt <timattt@gmail.com>
2022-07-27 18:07:36 +0300

    new trampoline texture

--- core/src/main/java/steelUnicorn/laplacity/field/tiles/TrampolineTile.java
+++ core/src/main/java/steelUnicorn/laplacity/field/tiles/TrampolineTile.java
@@ -17,17 +17,6 @@ public class TrampolineTile extends SolidTile {
 	@Override
 	public void constantDraw(SpriteCache sc) {
 		float sz = LaplacityField.tileSize;
-/*
-		boolean top = !(gridY < fieldHeight - 1) || tiles[gridX][gridY + 1] instanceof SolidTile;
-		boolean bottom = !(gridY > 0) || tiles[gridX][gridY - 1] instanceof SolidTile;
-		boolean right = !(gridX < fieldWidth - 1) || tiles[gridX + 1][gridY] instanceof SolidTile;
-		boolean left = !(gridX > 0) || tiles[gridX - 1][gridY] instanceof SolidTile;
-		
-		boolean topRight = !(gridX < fieldWidth - 1) || !(gridY < fieldHeight - 1) || tiles[gridX + 1][gridY + 1] instanceof SolidTile;
-		boolean bottomRight = !(gridX < fieldWidth - 1) || !(0 < gridY) || tiles[gridX + 1][gridY - 1]  instanceof SolidTile;
-		boolean topLeft = !(0 < gridX) || !(gridY < fieldHeight - 1) || tiles[gridX - 1][gridY + 1] instanceof SolidTile;
-		boolean bottomLeft = !(gridX > 0) || !(0 < gridY) || tiles[gridX - 1][gridY - 1] instanceof SolidTile;
-*/
 		sc.add(LaplacityAssets.TRAMPOLINE_REGION, gridX * sz, gridY * sz, sz/2, sz/2, sz, sz, 1, 1, 0);
 	}
 	
