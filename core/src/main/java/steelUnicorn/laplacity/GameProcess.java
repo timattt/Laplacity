229dc45b45025e1148bd089b200bf7dd7ea114e2
timattt <timattt@gmail.com>
2022-08-13 15:18:00 +0300

    rebalance

--- core/src/main/java/steelUnicorn/laplacity/GameProcess.java
+++ core/src/main/java/steelUnicorn/laplacity/GameProcess.java
@@ -132,7 +132,7 @@ public class GameProcess {
 	public static final float BRUSH_DENSITY_POWER = 0.05f * MAX_DENSITY;
 	
 	// TRAJECTORY
-	public static final int TRAJECTORY_POINTS = 50;
+	public static final int TRAJECTORY_POINTS = 20;
 	public static final int STEPS_PER_POINT = 2;
 	public static final float TRAJECTORY_VELOCITY_MULTIPLIER = 2f;
 	
