8cf7222a71eabb89c8e7b8b57f9a2906ef959063
timattt <timattt@gmail.com>
2022-08-15 13:03:11 +0300

    new deadly texture

--- core/src/main/java/steelUnicorn/laplacity/field/tiles/DeadlyTile.java
+++ core/src/main/java/steelUnicorn/laplacity/field/tiles/DeadlyTile.java
@@ -14,56 +14,12 @@ public class DeadlyTile extends SolidTile {
 		setId(4);
 	}
 
-	private int animIndex = 0;
-	
-	@Override
-	public TextureRegion getAnimatedRegion(float[] angle) {
-		boolean top = tiles[gridX][gridY + 1] instanceof DeadlyTile;
-		boolean bottom = tiles[gridX][gridY - 1] instanceof DeadlyTile;
-		boolean right = tiles[gridX + 1][gridY] instanceof DeadlyTile;
-		boolean left = tiles[gridX - 1][gridY] instanceof DeadlyTile;
-		
-		boolean topRight = !(gridX < fieldWidth - 1) || !(gridY < fieldHeight - 1) || tiles[gridX + 1][gridY + 1] instanceof DeadlyTile;
-		boolean bottomRight = !(gridX < fieldWidth - 1) || !(0 < gridY) || tiles[gridX + 1][gridY - 1]  instanceof DeadlyTile;
-		boolean topLeft = !(0 < gridX) || !(gridY < fieldHeight - 1) || tiles[gridX - 1][gridY + 1] instanceof DeadlyTile;
-		boolean bottomLeft = !(gridX > 0) || !(0 < gridY) || tiles[gridX - 1][gridY - 1] instanceof DeadlyTile;
-		
-		animIndex++;
-		animIndex %= 3;
-		
-		// wires
-		if (top && bottom && !left && !right) {
-			angle[0] = 90;
-			return LaplacityAssets.DEADLY_REGIONS[6 + (gridX + gridY + animIndex) % 3][0];
-		}
-		if (!top && !bottom && left && right) {
-			angle[0] = 0;
-			return LaplacityAssets.DEADLY_REGIONS[6 + (gridX + gridY + animIndex) % 3][0];
-		}
-		
-		// 3
-		if (!top && bottom && right && left && bottomLeft && bottomRight) {
-			angle[0] = 0;
-			return LaplacityAssets.DEADLY_REGIONS[6 + (gridX + gridY + animIndex) % 3][0];
-		}
-		if (top && !bottom && right && left && topLeft && topRight) {
-			angle[0] = 0;
-			return LaplacityAssets.DEADLY_REGIONS[6 + (gridX + gridY + animIndex) % 3][0];
-		}
-		if (top && bottom && !right && left && topLeft && bottomLeft) {
-			angle[0] = 90;
-			return LaplacityAssets.DEADLY_REGIONS[6 + (gridX + gridY + animIndex) % 3][0];
-		}
-		if (top && bottom && right && !left && topRight && bottomRight) {
-			angle[0] = 90;
-			return LaplacityAssets.DEADLY_REGIONS[6 + (gridX + gridY + animIndex) % 3][0];
-		}
-		
-		return null;
-	}
-
 	@Override
 	public TextureRegion getRegion(float[] angle) {
+		return texture2(angle);
+	}
+
+	private TextureRegion texture2(float[] angle) {
 		boolean top = tiles[gridX][gridY + 1] instanceof DeadlyTile;
 		boolean bottom = tiles[gridX][gridY - 1] instanceof DeadlyTile;
 		boolean right = tiles[gridX + 1][gridY] instanceof DeadlyTile;
@@ -77,62 +33,84 @@ public class DeadlyTile extends SolidTile {
 		// corners
 		if (top && bottom && right && left && !topRight && bottomRight && topLeft && bottomLeft) {
 			angle[0] = 180;
-			return LaplacityAssets.DEADLY_REGIONS[5][3];
+			return LaplacityAssets.DEADLY_REGIONS[6][2];
 		}
 		if (top && bottom && right && left && topRight && bottomRight && topLeft && !bottomLeft) {
 			angle[0] = 0;
-			return LaplacityAssets.DEADLY_REGIONS[5][3];
+			return LaplacityAssets.DEADLY_REGIONS[6][2];
 		}
 		if (top && bottom && right && left && topRight && !bottomRight && topLeft && bottomLeft) {
 			angle[0] = 90;
-			return LaplacityAssets.DEADLY_REGIONS[5][3];
+			return LaplacityAssets.DEADLY_REGIONS[6][2];
 		}
 		if (top && bottom && right && left && topRight && bottomRight && !topLeft && bottomLeft) {
 			angle[0] = -90;
-			return LaplacityAssets.DEADLY_REGIONS[5][3];
+			return LaplacityAssets.DEADLY_REGIONS[6][2];
 		}
+		//--------
+		// wires
+		if (top && bottom && !left && !right) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[7][1];
+		}
+		if (!top && !bottom && left && right) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[7][1];
+		}
+		
+		// 3
+		if (!top && bottom && right && left && bottomLeft && bottomRight) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[7][0];
+		}
+		if (top && !bottom && right && left && topLeft && topRight) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[7][0];
+		}
+		if (top && bottom && !right && left && topLeft && bottomLeft) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[7][0];
+		}
+		if (top && bottom && right && !left && topRight && bottomRight) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[7][0];
+		}
+		//-------
 		
 		// 1
 		if (top && !bottom && !left && !right) {
 			angle[0] = -90;
-			return LaplacityAssets.DEADLY_REGIONS[3][1];
+			return LaplacityAssets.DEADLY_REGIONS[4][0];
 		}
 		if (!top && bottom && !left && !right) {
 			angle[0] = 90;
-			return LaplacityAssets.DEADLY_REGIONS[3][1];
+			return LaplacityAssets.DEADLY_REGIONS[4][0];
 		}
 		if (!top && !bottom && left && !right) {
 			angle[0] = 0;
-			return LaplacityAssets.DEADLY_REGIONS[3][1];
+			return LaplacityAssets.DEADLY_REGIONS[4][0];
 		}
 		if (!top && !bottom && !left && right) {
 			angle[0] = 180;
-			return LaplacityAssets.DEADLY_REGIONS[3][1];
+			return LaplacityAssets.DEADLY_REGIONS[4][0];
 		}
 		
 		// 2
 		if (!top && bottom && right && !left) {
 			angle[0] = 90;
-			return LaplacityAssets.DEADLY_REGIONS[5][3];
+			return LaplacityAssets.DEADLY_REGIONS[6][2];
 		}
 		if (!top && bottom && !right && left) {
 			angle[0] = 0;
-			return LaplacityAssets.DEADLY_REGIONS[5][3];
+			return LaplacityAssets.DEADLY_REGIONS[6][2];
 		}
 		if (top && !bottom && !right && left) {
 			angle[0] = -90;
-			return LaplacityAssets.DEADLY_REGIONS[5][3];
+			return LaplacityAssets.DEADLY_REGIONS[6][2];
 		}
 		if (top && !bottom && right && !left) {
 			angle[0] = 180;
-			return LaplacityAssets.DEADLY_REGIONS[5][3];
-		}
-		// wires
-		if (top && bottom && !left && !right) {
-			return null;
-		}
-		if (!top && !bottom && left && right) {
-			return null;
+			return LaplacityAssets.DEADLY_REGIONS[6][2];
 		}
 		
 		// 3
@@ -142,7 +120,7 @@ public class DeadlyTile extends SolidTile {
 				return null;
 			} else {
 				angle[0] = 0;
-				return LaplacityAssets.DEADLY_REGIONS[4][0];
+				return LaplacityAssets.DEADLY_REGIONS[4][1];
 			}
 		}
 		if (top && !bottom && right && left) {
@@ -151,7 +129,7 @@ public class DeadlyTile extends SolidTile {
 				return null;
 			} else {
 				angle[0] = 180;
-				return LaplacityAssets.DEADLY_REGIONS[4][0];
+				return LaplacityAssets.DEADLY_REGIONS[4][1];
 			}
 		}
 		if (top && bottom && !right && left) {
@@ -160,7 +138,7 @@ public class DeadlyTile extends SolidTile {
 				return null;
 			} else {
 				angle[0] = -90;
-				return LaplacityAssets.DEADLY_REGIONS[4][0];
+				return LaplacityAssets.DEADLY_REGIONS[4][1];
 			}
 		}
 		if (top && bottom && right && !left) {
@@ -169,7 +147,147 @@ public class DeadlyTile extends SolidTile {
 				return null;
 			} else {
 				angle[0] = 90;
-				return LaplacityAssets.DEADLY_REGIONS[4][0];
+				return LaplacityAssets.DEADLY_REGIONS[4][1];
+			}
+		}
+	
+		// 4
+		if (top && bottom && right && left) {
+			return null;
+		}
+		
+		return LaplacityAssets.DEADLY_REGIONS[0][0];
+	}
+	
+	private TextureRegion texture1(float[] angle) {
+		boolean top = tiles[gridX][gridY + 1] instanceof DeadlyTile;
+		boolean bottom = tiles[gridX][gridY - 1] instanceof DeadlyTile;
+		boolean right = tiles[gridX + 1][gridY] instanceof DeadlyTile;
+		boolean left = tiles[gridX - 1][gridY] instanceof DeadlyTile;
+		
+		boolean topRight = !(gridX < fieldWidth - 1) || !(gridY < fieldHeight - 1) || tiles[gridX + 1][gridY + 1] instanceof DeadlyTile;
+		boolean bottomRight = !(gridX < fieldWidth - 1) || !(0 < gridY) || tiles[gridX + 1][gridY - 1]  instanceof DeadlyTile;
+		boolean topLeft = !(0 < gridX) || !(gridY < fieldHeight - 1) || tiles[gridX - 1][gridY + 1] instanceof DeadlyTile;
+		boolean bottomLeft = !(gridX > 0) || !(0 < gridY) || tiles[gridX - 1][gridY - 1] instanceof DeadlyTile;
+		
+		// corners
+		if (top && bottom && right && left && !topRight && bottomRight && topLeft && bottomLeft) {
+			angle[0] = 180;
+			return LaplacityAssets.DEADLY_REGIONS[1][3];
+		}
+		if (top && bottom && right && left && topRight && bottomRight && topLeft && !bottomLeft) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[1][3];
+		}
+		if (top && bottom && right && left && topRight && !bottomRight && topLeft && bottomLeft) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[1][3];
+		}
+		if (top && bottom && right && left && topRight && bottomRight && !topLeft && bottomLeft) {
+			angle[0] = -90;
+			return LaplacityAssets.DEADLY_REGIONS[1][3];
+		}
+		//--------
+		// wires
+		if (top && bottom && !left && !right) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[7][1];
+		}
+		if (!top && !bottom && left && right) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[7][1];
+		}
+		
+		// 3
+		if (!top && bottom && right && left && bottomLeft && bottomRight) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[7][1];
+		}
+		if (top && !bottom && right && left && topLeft && topRight) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[7][1];
+		}
+		if (top && bottom && !right && left && topLeft && bottomLeft) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[7][1];
+		}
+		if (top && bottom && right && !left && topRight && bottomRight) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[7][1];
+		}
+		//-------
+		
+		// 1
+		if (top && !bottom && !left && !right) {
+			angle[0] = -90;
+			return LaplacityAssets.DEADLY_REGIONS[2][1];
+		}
+		if (!top && bottom && !left && !right) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[2][1];
+		}
+		if (!top && !bottom && left && !right) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[2][1];
+		}
+		if (!top && !bottom && !left && right) {
+			angle[0] = 180;
+			return LaplacityAssets.DEADLY_REGIONS[2][1];
+		}
+		
+		// 2
+		if (!top && bottom && right && !left) {
+			angle[0] = 90;
+			return LaplacityAssets.DEADLY_REGIONS[1][3];
+		}
+		if (!top && bottom && !right && left) {
+			angle[0] = 0;
+			return LaplacityAssets.DEADLY_REGIONS[1][3];
+		}
+		if (top && !bottom && !right && left) {
+			angle[0] = -90;
+			return LaplacityAssets.DEADLY_REGIONS[1][3];
+		}
+		if (top && !bottom && right && !left) {
+			angle[0] = 180;
+			return LaplacityAssets.DEADLY_REGIONS[1][3];
+		}
+		
+		// 3
+		if (!top && bottom && right && left) {
+			if (bottomLeft && bottomRight) {
+				angle[0] = 0;
+				return null;
+			} else {
+				angle[0] = 0;
+				return LaplacityAssets.DEADLY_REGIONS[0][1];
+			}
+		}
+		if (top && !bottom && right && left) {
+			if (topLeft && topRight) {
+				angle[0] = 180;
+				return null;
+			} else {
+				angle[0] = 180;
+				return LaplacityAssets.DEADLY_REGIONS[0][1];
+			}
+		}
+		if (top && bottom && !right && left) {
+			if (topLeft && bottomLeft) {
+				angle[0] = -90;
+				return null;
+			} else {
+				angle[0] = -90;
+				return LaplacityAssets.DEADLY_REGIONS[0][1];
+			}
+		}
+		if (top && bottom && right && !left) {
+			if (topRight && bottomRight) {
+				angle[0] = 90;
+				return null;
+			} else {
+				angle[0] = 90;
+				return LaplacityAssets.DEADLY_REGIONS[0][1];
 			}
 		}
 	
