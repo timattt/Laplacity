22d97c8d48f2959544a5b1e77869394c79d13a26
EKhunshaev <elveg100@gmail.com>
2022-08-23 13:04:13 +0300

    ui - win interface redesign; background fix

--- core/src/main/java/steelUnicorn/laplacity/screens/WinScreen.java
+++ core/src/main/java/steelUnicorn/laplacity/screens/WinScreen.java
@@ -38,7 +38,7 @@ public class WinScreen extends ManagedScreen {
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1f, 1f, 1f, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		winStage.act();
