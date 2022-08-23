bed7256cc7c0abc45800b7a0da7d92495f3908b2
EKhunshaev <elveg100@gmail.com>
2022-08-23 17:44:29 +0300

    next button at story

--- core/src/main/java/steelUnicorn/laplacity/screens/StoryScreen.java
+++ core/src/main/java/steelUnicorn/laplacity/screens/StoryScreen.java
@@ -5,12 +5,17 @@ import static steelUnicorn.laplacity.core.LaplacityAssets.*;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.TimeUtils;
 
 import de.eskalon.commons.screen.ManagedScreen;
 import steelUnicorn.laplacity.GameProcess;
+import steelUnicorn.laplacity.core.LaplacityAssets;
 import steelUnicorn.laplacity.utils.LevelsParser;
 
 public class StoryScreen extends ManagedScreen {
@@ -18,12 +23,17 @@ public class StoryScreen extends ManagedScreen {
 	private Stage loadingStage;
 	private Image storyImage;
 	private int storyIndex;
-	
+
+	private TextButton nextBtn;
+	private static float nextBtnPad = UI_WORLD_HEIGHT * 0.05f;
+	private static float nextScaleX = 2f;
+	private static float nextScaleY = 1.2f;
+
 	private boolean requestedChange = false;
 	private long start = 0;
 	
 	private static final long waitTime = 3000;
-	
+
 	public StoryScreen(int index) {
 		this.storyIndex = index;
 		
@@ -34,10 +44,28 @@ public class StoryScreen extends ManagedScreen {
 				guiViewport.getWorldHeight());
 		storyImage.setPosition(-storyImage.getWidth() / 2 + guiViewport.getWorldWidth() / 2, 0);
 		loadingStage.addActor(storyImage);
+
+		nextBtn = new TextButton("Next", TEXSKIN);
+		nextBtn.setVisible(false);
+		nextBtn.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				LaplacityAssets.playSound(clickSound);
+				next();
+			}
+		});
+
+		Table root = new Table();
+		root.setFillParent(true);
+		root.add(nextBtn).expand().right().bottom()
+				.size(nextBtn.getPrefWidth() * nextScaleX, nextBtn.getPrefHeight() * nextScaleY)
+				.pad(nextBtnPad);
+		loadingStage.addActor(root);
 	}
 
 	@Override
 	protected void create() {
+		this.addInputProcessor(loadingStage);
 	}
 	
 	private void next() {
@@ -58,18 +86,19 @@ public class StoryScreen extends ManagedScreen {
 
 	@Override
 	public void hide() {
+		nextBtn.setVisible(false);
 	}
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1f, 1f, 1f, 1);
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		loadingStage.draw();
 		loadingStage.act();
 		
 		if (TimeUtils.millis() - start > waitTime) {
-			next();
+			nextBtn.setVisible(true);
 		}
 	}
 
@@ -87,6 +116,7 @@ public class StoryScreen extends ManagedScreen {
 	
 	@Override
 	public void show() {
+		super.show();
 		requestedChange = false;
 		start = TimeUtils.millis();
 	}
