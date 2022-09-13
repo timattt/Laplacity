03feb47a7f3017f9cabc8404ba62663ab791e18e
EKhunshaev <elveg100@gmail.com>
2022-09-13 22:40:44 +0300

    lower bound for restore time notification

--- core/src/main/java/steelUnicorn/laplacity/utils/NotificationHandler.java
+++ core/src/main/java/steelUnicorn/laplacity/utils/NotificationHandler.java
@@ -1,9 +1,12 @@
 package steelUnicorn.laplacity.utils;
 
+import steelUnicorn.laplacity.core.Laplacity;
+
 /**
  * Интерфейс для обработки уведомлений. Нужен для хранения параметров уведомлений, и их создания.
  */
 public interface NotificationHandler {
+    int RESTORE_LOWER_BOUND = Laplacity.isDebugEnabled() ? 10 * 60 : 30 * 60; //10 min in debug and 3 food in release
     /**
      * Устанавливает AlarmManager для уведомления о восстановлении еды у кота.
      */
